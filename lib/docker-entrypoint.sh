16c6ef31f2d2e059b2643e5bc6df6fa5fbcd0fc7
Jonathan Duggan <jonathan@merxnet.io>
2018-07-31 16:20:22 -0400

    initial commit

---
+++ lib/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+## cryptoforknote-util and cryptonight-hashing have to be
+## recompiled for each host for some unknown reason; otherwise,
+## node exits with "Illegal instruction (core dumped)" error code 132
+echo "Rebuilding dependencies for host (this may take some time)..."
+npm rebuild --quiet
+npm cache clean --force --quiet
+
+exec "$@"
